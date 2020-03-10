798345982b3d1fae1027b77b9765806654fc205b
Anthony K <antonyk@users.noreply.github.com>
2020-03-10 18:54:32 -0400

    rename less files; add second toolbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
 // Your code goes here
+// console.log("something!")
+
+
+
+// IDEAS:
+// analytics - user hovers over a mouse, save data
+// 
+
+
+function clickEventHandler(event) {
+  event.preventDefault();
+  // event.stopPropagation();
+  console.log(`${event.type} !!!`);
+  console.log(event.target);
+  console.log(event.currentTarget);
+}
+
+homeLink.addEventListener(
+  'click', // string with the name of the event
+  clickEventHandler
+);
+
+document.querySelector('nav').addEventListener(
+  'click',
+  clickEventHandler, true
+);
+document.querySelector('nav').addEventListener(
+  'click',
+  clickEventHandler
+);
+document.querySelector('nav').addEventListener(
+  'click',
+  clickEventHandler
+);
+
+document.querySelector('header').addEventListener(
+  'click',
+  clickEventHandler
+);
+
+// document.body.addEventListener(
+//     'click',
+//     clickEventHandler
+// );
+  
+// document.addEventListener(
+//   'click',
+//   clickEventHandler
+// );
+
+// window.addEventListener(
+//   'click',
+//   clickEventHandler
+// );
+
+
+
+// javascript pocket edition by o'reiley
+//
+
